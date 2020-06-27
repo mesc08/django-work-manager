26dd7763c5d4e10f415ae0186ec35987cd71f34c
SRIJAN CHAKRABORTY <srijanch0810@gmail.com>
2020-06-27 17:21:37 +0530

    10th update

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout, authenticate 
 from django.utils import timezone
 from .forms import TodoForm
 from .models import todolist
 from django.contrib.auth.decorators import login_required
-#import pdfkit
+
 # Create your views here.
 def home(request):
 	return render(request,'todolist/home.html')
@@ -59,7 +59,7 @@ def createtodolist(request):
 		return render(request,'todolist/createtodolist.html',{'form':TodoForm()})
 	else:
 		try:
-			todo = TodoForm()
+			todo = TodoForm(request.POST)
 			new_todo = todo.save(commit=False)
 			new_todo.user = request.user
 			new_todo.save()
@@ -97,7 +97,7 @@ def completetodolist(request,todolist_pk):
 	todo = get_object_or_404(todolist, pk = todolist_pk, user = request.user)	
 	if request.method == "POST":
 		todo.datedcompleted = timezone.now()
-		todolist.save()
+		todo.save()
 		return redirect('currenttodolist')
 
 @login_required
